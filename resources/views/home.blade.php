9082753440083b067b7f9fb7e69761b87ae91e34
mguggiari <micaela.guggiari@davinci.edu.ar>
2023-05-18 12:16:49 -0300

    csss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,17 +6,19 @@
 @extends('layouts.main')
 
 @section('header-hero')
-    <div class="col-12 col-md-5 p-3 text-right">
-        <label class="text-uppercase text-secondary-emphasis fw-medium">crea tu propio sitio con</label>
-        <h1 class="text-uppercase text-white fs-2">
-            premium hosting
-        </h1>
-        <p class="text-uppercase text-secondary-emphasis fw-medium">
-            minitexto                         
-        </p>
-        <button class="btn btn-danger">Ver Productos</button>
+    <div class="col-12 col-md-5 p-3 text-right d-flex  justify-content-center justify-content-md-end align-items-center">
+        <div>
+            <label class="text-uppercase text-secondary-emphasis fw-medium">crea tu propio sitio con</label>
+            <h1 class="text-uppercase text-white fs-2">
+                premium hosting
+            </h1>
+            <p class="text-uppercase text-secondary-emphasis fw-medium">
+                minitexto                         
+            </p>
+            <button class="btn btn-danger">Ver Productos</button>
+        </div>
     </div>
-    <div class="col-12 col-md-7">
+    <div class="col-12 col-md-7 d-flex justify-content-center justify-content-md-start">
         <picture>
             <source  srcset="{{ url('img/section-uno-desktop.png') }}" media="(min-width:768px)">
             <img class="img-fluid" alt="persona mirando compu creando una idea" src="{{ url('img/section-uno-mobile.png') }}">
@@ -26,7 +28,7 @@
 
 @section('main')
 <div class="col-12">
-    <h2 class="text-center mt-3">¿Porqué Elegirnos?</h2>
+    <h2 class="text-center">¿Porqué Elegirnos?</h2>
 </div>
 <div class="col-12 mt-3">
     <div class="d-flex justify-content-evenly flex-column flex-md-row align-items-center">
@@ -34,13 +36,13 @@
             <img src="{{ url('img/server.png') }}" alt="nuve atendiendo servidor">
             <p class="fw-semibold text-center mt-2">99.9% Garantía de tiempo de actividad</p>
         </div>
-        <div class="card-body-dos mt-3 mt-md-0">
-            <img src="{{ url('img/encrypted.png') }}" alt="hosting seguro">
-            <p class="fw-semibold mt-2">Seguro y Protegido</p>
+        <div class="card-body-dos mt-2 mt-md-0">
+            <img src="{{ url('img/encrypted.png') }}" class="card-img-top" alt="...">
+            <p>Seguro y Protegido</p>
         </div>
-        <div class="card-body-dos mt-3 mt-md-0">
-            <img src="{{ url('img/helpline.png') }}" alt="ayuda en linea y telefono">
-            <p class="fw-semibold mt-2">Nuestro Apoyo Dedicado</p>
+        <div class="card-body-dos mt-2 mt-md-0">
+            <img src="{{ url('img/helpline.png') }}" class="card-img-top" alt="...">
+            <p>Nuestro Apoyo Dedicado</p>
         </div>
     </div>
 </div>
