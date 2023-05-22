3e308f862a4b2ee4bcde9bd1430ae03508e13806
mguggiari <micaela.guggiari@davinci.edu.ar>
2023-05-22 18:15:09 -0300

    404 update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,12 +5,21 @@
 
 
 @section('main')
-
-<p>
-    Página no encontrada.
-</p>
-
-<a href="{{ route('home') }}" role="button" class="btn btn-success">
-    Ir al Sitio Web 
-</a>
+<section class="row">
+    <div class="col-12 col-md-5 p-3 text-right d-flex  justify-content-center justify-content-md-end align-items-center">
+        <div>
+            <h1 class="fs-2">
+                ¡Ups! Ha ocurrido un error...
+            </h1>
+            <p>Te invitamos a ver nuestros últimos posteos mientras trabajamos en ello</p>
+            <button class="btn btn-dashboard">Ver posteos</button>
+        </div>
+    </div>
+    <div class="col-12 col-md-7 d-flex justify-content-center justify-content-md-start">
+        <picture>
+            <source srcset="{{ url('img/error-desktop.jpg') }}" media="(min-width:768px)">
+            <img class="img-fluid" alt="persona mirando compu creando una idea" src="{{ url('img/error-mobile.jpg') }}">
+        </picture>
+    </div>
+</section>
 @endsection
