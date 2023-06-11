ab89b5564c18c68ce2c3c83329eee575e28abe6c
mguggiari <micaela.guggiari@davinci.edu.ar>
2023-06-11 15:53:35 -0300

    listados check

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,11 +7,7 @@
 
 @section('main')
 <section  class="section-blog">
-    <div class="row">
-        <div class="col-12">
-            <h1 class="mb-3 mt-3 blog">Nuestro Blog de Noticias</h1>
-        </div>
-        @include('posts._allpost-data')
-    </div>
+    <h1 class="mb-3 mt-3 blog">Nuestro Blog de Noticias</h1>
+    @include('posts._allpost-data')
 </section>
 @endsection
--- resources/views/posts/_allpost-data.blade.php
+++ resources/views/posts/_allpost-data.blade.php
@@ -1,28 +1,27 @@
 <?php
 /** @var \App\Models\Post[]|\Illuminate\Database\Eloquent\Collection $posts
- *
  * use _ to indicate that this is a partial view
- * 
  */
 use Illuminate\Support\Str;
 ?>
+<ul class="list-unstyled d-md-flex justify-content-md-around row p-3 p-md-0">
 @foreach ($posts as $post)
-<div class="col-12 col-md-4 mt-3 mb-3">
-    <div class="card">
-        <div class="card-body">
-            <div class="fw-medium text-secondary mb-2">
-                {{ $post->category->name }}
-            </div>
-            <h2 class="card-title" > {{ $post->title }} </h2>
-            <div class="fw-medium text-secondary mb-2">Autor:
-                {{ $post->author->name }}
-            </div>
-            <p class="card-text">{{ Str::words($post->content, 30) }}</p>
-            <a href="{{ route('posts.fullpost', ['id' => $post->id]) }}" class="btn btn-entradas float-end">Ver más</a>
+<li class="mt-3 mb-3 me-md-3 col-12 col-md-5 card">
+    <div class="card-body">
+        <div class="fw-medium text-secondary mb-2">
+            {{ $post->category->name }}
         </div>
+        <h2 class="card-title" > {{ $post->title }} </h2>
+        <div class="fw-medium text-secondary mb-2">Autor:
+            {{ $post->author->name }}
+        </div>
+        <p class="card-text">{{ Str::words($post->content, 30) }}</p>
+        <a href="{{ route('posts.fullpost', ['id' => $post->id]) }}" class="btn btn-entradas float-end">Ver más</a>
     </div>
-</div>
+</li>
 @endforeach
+</ul>
+
 {{-- <div class="card">
     <div>
         imagen
