d48355be5a66cc78a195a1a4409a3c4a72fd3f3a
mguggiari <micaela.guggiari@davinci.edu.ar>
2023-06-17 18:59:52 -0300

    css mail

--- resources/views/products/confirmContractProduct.blade.php
+++ resources/views/products/confirmContractProduct.blade.php
@@ -2,14 +2,13 @@
 @section('main')
 <div class="div-confirm-product mt-5">
     <h1 class="fs-2 text-center blog visually-hidden">Confirmar Reserva</h1>
-    <p class="text-center titulo-admin">Estás a punto de reservar el siguiente producto:</p>
+    <p class="text-center titulo-admin fw-bold">Estás a punto de reservar el siguiente producto:</p>
     <h2 class="text-center">{{ $product->title }}</h2>
     <p>{{ $product->description }}</p>
     <form action="{{ route('products.processContractProduct', $product->product_id) }}" method="POST" class="d-flex justify-content-center">
         @csrf
-        <button type="submit" class="btn btn-hostin">Confirmar Contratación</button>
+        <button type="submit" class="btn btn-hostin">Confirmar Reserva</button>
     </form>
     <p class="form-aclaracion text-center mt-3">Recibirás con un mail con las indicaciones para finalizar el proceso de reserva</p>
-
 </div>
 @endsection
