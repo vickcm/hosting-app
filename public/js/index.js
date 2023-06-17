a988ffacea8d43bf2edf868b29ec0e48fe7ef45a
vickcm <77595917+vickcm@users.noreply.github.com>
2023-06-17 18:59:34 -0300

    updates

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,21 @@
 function togglePasswordVisibility() {
-    const passwordInput = document.getElementById("password");
-    const passwordToggle = document.querySelector(".password-toggle");
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
+    var passwordInput = document.getElementById('password');
+    var passwordToggleOpenIcon = document.querySelector('.password-toggle-open-icon');
+    var passwordToggleSlashIcon = document.querySelector('.password-toggle-slash-icon');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        passwordToggleOpenIcon.style.display = 'none';
+        passwordToggleSlashIcon.style.display = 'block';
     } else {
-        passwordInput.type = "password";
+        passwordInput.type = 'password';
+        passwordToggleOpenIcon.style.display = 'block';
+        passwordToggleSlashIcon.style.display = 'none';
     }
 }
+
+// Ocultar el ícono de la contraseña visible al cargar la página
+document.addEventListener('DOMContentLoaded', function() {
+    var passwordToggleOpenIcon = document.querySelector('.password-toggle-open-icon');
+    passwordToggleOpenIcon.style.display = 'none';
+});
