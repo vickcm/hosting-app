a4ca8865c2559892986acc80fb35f2956ae68a12
vickcm <77595917+vickcm@users.noreply.github.com>
2023-07-30 18:33:57 -0300

    updates profiles

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Profile;
+use Carbon\Carbon;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    //view Profile
+    public function viewProfile()
+    {
+        // Obtener el usuario autenticado
+        $user = auth()->user();
+
+        // Obtener el perfil asociado al usuario (si existe)
+        $profile = $user->profile;
+
+        // Formatear la fecha de nacimiento (si existe)
+        if ($profile) {
+            $profile->formatted_birth_date = Carbon::parse($profile->birth_date)->format('d/m/Y');
+        }
+        // Obtener los productos contratados por el usuario que no esten cancelados, chequear status, campo boolean true o false. True para vigentes, false para cancelados
+
+        $contractedProducts = $user->products()->wherePivot('status', true)->get();
+
+        return view('profiles.viewProfile', compact('user', 'profile', 'contractedProducts'));
+    }
+
+
+
+
+}
