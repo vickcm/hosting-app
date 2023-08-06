9a8539f4ef2946916b4247b53421a68064fa8bac
mguggiari <micaela.guggiari@davinci.edu.ar>
2023-08-06 17:29:31 -0300

    confirm delete

--- resources/views/products/confirmCancelProduct.blade.php
+++ resources/views/products/confirmCancelProduct.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.main')
 
-@section('title', 'Confirmar Reserva :: Nube web')
+@section('title', 'Confirmar Cancelación de Suscripción :: Nube web')
 
 @section('main')
 <div class="div-confirm-product mt-5">
-    <h1 class="fs-2 text-center blog visually-hidden">Confirmar Reserva</h1>
-    <p class="text-center titulo-admin fw-bold">Estás a punto de reservar el siguiente producto:</p>
+    <h1 class="fs-2 text-center titulo-cancelar">Confirmar Cancelación de Suscripción</h1>
     <h2 class="text-center">{{ $product->title }}</h2>
     <p>{{ $product->description }}</p>
     <form action="{{ route('products.confirmCancelProduct', $product->product_id) }}" method="POST" class="d-flex justify-content-center">
         @csrf
-        <button type="submit" class="btn btn-hostin">confirmo</button>
+        <button type="submit" class="btn btn-cancelar">Confirmo Cancelación</button>
     </form>
-    <p class="form-aclaracion text-center mt-3">Recibirás con un mail con las indicaciones para finalizar el proceso de reserva</p>
+</div>
+<div class="d-flex justify-content-center mt-3">
+    <a href="{{ route('profiles.viewProfile') }}" class="btn">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/></svg>
+        Volver a Mi Cuenta
+    </a>
 </div>
 @endsection
